eee7565abf7962223d8acf2cc92c28805fd7b3c5
Max <max@maxf.net>
2023-05-05 09:44:11 -0400

    loop dev mount up

--- main/docker-entrypoint.sh
+++ main/docker-entrypoint.sh
@@ -4,14 +4,21 @@ ls /etc/ssh/ssh_host_*key* > /dev/null || ssh-keygen -A
 
 /usr/sbin/sshd -D &
 
-DATA_IMG=/mnt/data/data.img
-if [ -z $DATA_IMG ]; then
+DATA_IMG=/mnt/host/data.img
+if [ ! -f $DATA_IMG ]; then
 	dd if=/dev/zero of=$DATA_IMG bs=1G count=1
 fi
 
 if ! file $DATA_IMG | grep -q ext4; then
 	mkfs.ext4 $DATA_IMG
 fi
+
+losetup -a | grep -q ^$DPSRV_DEV_LOOP || losetup -P $DPSRV_DEV_LOOP $DATA_IMG
+
+DATA_MNT=/mnt/data
+
+[ -d $DATA_MNT ] || mkdir $DATA_MNT
+mount -o loop $DPSRV_DEV_LOOP $DATA_MNT
 
 wait -n
 rc=$?
