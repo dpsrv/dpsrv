7db5b2fb5c7cea23a2ae1bccf3c2fdb92cd98d60
Max <max@maxf.net>
2023-05-11 12:54:24 -0400

    updated

--- host/init/01_drbd.sh
+++ host/init/01_drbd.sh
@@ -5,46 +5,67 @@ apk add drbd lsblk
 DRBD_IMG=${DRBD_IMG:-/vagrant/.vagrant/drbd.img}
 
 if [ ! -f $DRBD_IMG ]; then
-	dd if=/dev/zero of=$DRBD_IMG bs=100M count=1
+	dd if=/dev/zero of=$DRBD_IMG bs=100M count=1 
 fi
 
-DRBD_DEV=$(losetup -a | grep $DRBD_IMG\$ | cut -d: -f1)
-if [ -z "$DRBD_DEV" ]; then
+DRBD_LOOP_DEV=$(losetup -a | grep $DRBD_IMG\$ | cut -d: -f1)
+if [ -z "$DRBD_LOOP_DEV" ]; then
 	losetup -Pf $DRBD_IMG
-	DRBD_DEV=$(losetup -a | grep $DRBD_IMG\$ | cut -d: -f1)
+	DRBD_LOOP_DEV=$(losetup -a | grep $DRBD_IMG\$ | cut -d: -f1)
 fi
 
-hostname=$(hostname)
-DRBD_ADDRS=${DPSRV_DRBD_ADDRS:-$hostname:7789}
+DRBD_DEV=/dev/drbd0
 
-export DRBD_DOMAINS=$(
-	i=0
-	for addr in $DRBD_ADDRS; do
-		host=${addr%:*}
-		port=${addr#*:}
-		if [ "$host" = "$hostname" ]; then
-			ipv4=$(ifconfig eth0 | grep 'inet addr:' |cut -d: -f2|awk '{ print $1 }')
-		else
-			ipv4=$(getent ahostsv4 $host|awk '{ print $1 }'|sort -fu)
-		fi
+if [ ! -f /vagrant/.vagrant/drbd.res ]; then
+
+	hostname=$(hostname)
+	DRBD_ADDRS=${DPSRV_DRBD_ADDRS:-$hostname:7789 localhost:7789}
+
+	export DRBD_DOMAINS=$(
+		i=0
+		for addr in $DRBD_ADDRS; do
+			host=${addr%:*}
+			port=${addr#*:}
+			if [ "$host" = "$hostname" ]; then
+				ipv4=$(ifconfig eth0 | grep 'inet addr:' |cut -d: -f2|awk '{ print $1 }')
+			else
+				ipv4=$(getent ahostsv4 $host|awk '{ print $1 }'|sort -fu)
+			fi
 	
-		cat <<_EOT_
+			cat <<_EOT_
 	on $host {
 		address $ipv4:$port;
 	}
 _EOT_
-		i=$(( i + 1 ))
-	done
-)
+			i=$(( i + 1 ))
+		done
+	)
 
-cat <<_EOT_> /etc/drbd.d/drbd.res
+cat <<_EOT_> /vagrant/.vagrant/drbd.res
 resource drbd {
 	device minor 0;
-	disk $DRBD_DEV;
+	disk $DRBD_LOOP_DEV;
 	meta-disk internal;
 	protocol A;
  
 $DRBD_DOMAINS
 }
 _EOT_
-   
+
+	cp /vagrant/.vagrant/drbd.res /etc/drbd.d/drbd.res
+	drbdadm -v create-md drbd
+else
+	cp /vagrant/.vagrant/drbd.res /etc/drbd.d/drbd.res
+fi
+
+drbdadm -v up drbd
+drbdadm -v primary drbd
+# drbdadm -v primary drbd --force
+
+if ! file $DRBD_DEV | grep -q ext4; then
+   	mkfs.ext4 $DRBD_DEV
+fi
+
+DRBD_MNT=/mnt/drbd
+mkdir -p $DRBD_MNT
+mount $DRBD_DEV $DRBD_MNT
