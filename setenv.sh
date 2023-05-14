66c3ec68d1bba9c609285775994014dc84c1f6c9
Max <max@maxf.net>
2023-05-14 16:02:15 -0400

    updated

--- setenv.sh
+++ setenv.sh
@@ -4,3 +4,11 @@ cd $OLDPWD
 
 export DPSRV_MNT=$DPSRV_ROOT/mnt
 
+export GIT_FILTER_OPENSSL_PREFIX=$HOME/.config/git/dpsrv-openssl-
+[ -f ${GIT_FILTER_OPENSSL_PREFIX}salt ] || openssl rand -hex 8 > ${GIT_FILTER_OPENSSL_PREFIX}salt
+[ -f ${GIT_FILTER_OPENSSL_PREFIX}password ] || openssl rand -hex 32 > ${GIT_FILTER_OPENSSL_PREFIX}password
+
+if [ ! -d $DPSRV_ROOT/.git/filter/openssl ]; then
+	git-init-openssl-secrets.sh 
+fi
+
