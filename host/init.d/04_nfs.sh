cf3ef863e25067b4cc905a2a20fa83511517f04b
Max <max@maxf.net>
2023-09-27 10:34:47 -0400

    updated

--- host/init.d/04_nfs.sh
+++ host/init.d/04_nfs.sh
@@ -1,15 +1,4 @@
 #!/bin/ash -ex
-exit 
 
-apk add nfs-utils
-
-[ -d /vagrant/.vagrant/nfs ] || mkdir /vagrant/.vagrant/nfs
-
-cat <<_EOT_>> /etc/exports
-/vagrant/.vagrant/nfs *(rw,sync,no_root_squash,no_subtree_check)
-_EOT_
-
-rc-update add nfs
-rc-service nfs start
-
+grep " nfs " /etc/mtab >> /etc/fstab
 
