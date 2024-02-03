39d81d535dd451aeb255a81e18cd4076848c6475
Max Fortun <max@maxf.net>
2024-02-03 16:06:51 -0500

    updated

--- host/init.d/05_dpsrv.sh
+++ host/init.d/05_dpsrv.sh
@@ -1,4 +1,4 @@
 #!/bin/ash -ex
 
 apk add git
-sudo docker ash -c 'mkdir projects && cd projects && git clone https://github.com/dpsrv/rc.git'
+sudo -u docker ash -c 'mkdir projects && cd projects && git clone https://github.com/dpsrv/rc.git'
